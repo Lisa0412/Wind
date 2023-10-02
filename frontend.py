c7c4a289d4e0f6cbb0f0d9e87647dce30309ee07
Lisa <burdina.marina@gmail.com>
2023-10-02 15:50:01 +0300

    Continue to work on frontend

--- frontend.py
+++ frontend.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 from forms import WeatherData
 from flask import request
-
+from src import weathercode_dictionary
 
 app = Flask(__name__)
 
@@ -9,7 +9,13 @@ app = Flask(__name__)
 def weather_forecast():
     form = WeatherData()
     if request.method =='POST':
-        print(request.form['latitude'], request.form['longitude'])
+        input_list = [request.form['latitude'], request.form['longitude']]
+        latitude_longitude_dictionary_input = {"latitude": input_list[0], "longitude": input_list[1]}
+        print(latitude_longitude_dictionary_input)
+
+        with open("src/Script_V1_python.py", mode="r", encoding="utf-8") as forecast:
+          code = forecast.read()
+        print(exec(code, {"latitude_longitude_dictionary" : latitude_longitude_dictionary_input}))
     return render_template('home.html', form = form)
 
 
--- src/Script_V1_python.py
+++ src/Script_V1_python.py
@@ -1,13 +1,13 @@
 import requests
-from weathercode_dictionary import weathercode_dictionary
+#from weathercode_dictionary import weathercode_dictionary_codes
 
-user_input = input("Введіть координати в десяткових градусах через кому, приклад: 49.хххх, 24.хххх\n")
-input_list = user_input.split(",")
-if len(input_list) != 2:
-    print("\nневірний формат вводу")
-    exit(1)
+#user_input = input("Введіть координати в десяткових градусах через кому, приклад: 49.хххх, 24.хххх\n")
+#input_list = user_input.split(",")
+#if len(input_list) != 2:
+ #   print("\nневірний формат вводу")
+ #   exit(1)
 
-latitude_longitude_dictionary = {"latitude": input_list[0], "longitude": input_list[1]}
+#latitude_longitude_dictionary = {"latitude": input_list[0], "longitude": input_list[1]}
 
 
 def validate_and_execute(latitude_longitude_dictionary):
@@ -33,7 +33,38 @@ weather_value = API_data['current_weather']
 #print(weather_value)
 #print(weather_value_detailed)
 
-print(" Погода: ", weathercode_dictionary[str(weather_value['weathercode'])], "\n"
+weathercode_dictionary_codes = {"0": "Ясно",
+                          "1": "Переважно ясно",
+                          "2": "Частково хмарно",
+                          "3": "Хмарно",
+                          "45": "Туман",
+                          "48": "Туман, іней",
+                          "51": "Слабка мряка",
+                          "53": "Помірна мряка",
+                          "55": "Сильна мряка",
+                          "56": "Крижаний дощ (невисока інтенсивність)",
+                          "57": "Крижаний дощ",
+                          "61": "Слабкий дощ",
+                          "63": "Дощ",
+                          "65": "Злива",
+                          "66": "Крижаний дощ",
+                          "67": "Крижана злива",
+                          "71": "Снігопад (легкий)",
+                          "73": "Снігопад (помірний)",
+                          "75": "Снігопад (сильний)",
+                          "77": "Град",
+                          "80": "Злива",
+                          "81": "Злива",
+                          "82": "Злива",
+                          "85": "Злива",
+                          "86": "Злива",
+                          "95": "Гроза",
+                          "96": "Гроза",
+                          "99": "Гроза",
+                          }
+
+
+print(" Погода: ", weathercode_dictionary_codes[str(weather_value['weathercode'])], "\n"
       " Температура повітря: ", weather_value['temperature'], "°C\n",
       "Середня швидкість вітру: ", weather_value['windspeed'], "м/с\n",
       "Напрям вітру: ", weather_value['winddirection'], "°\n"
